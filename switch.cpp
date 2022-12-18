4980bf743330fdfb49c5455695d646c2bcad0b23
Nilay Rawal <rawalnilay1@gmail.com>
2022-12-18 22:25:52 +0530

    day 4

---
+++ switch.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main (){
+    char ch =4;
+
+    switch (ch)
+    {
+    case 1:cout<<"first";      
+        /* code */
+        break;
+
+    case 2:
+    cout<<"sencond";
+    break;
+    
+    default:
+    cout<<"it is default case"<<endl;
+        break;
+    }
+    
+
+
+    return 0;
+}
