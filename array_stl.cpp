948b0d57bed038577a86f4b94fa58069198b0e64
Nilay Rawal <rawalnilay1@gmail.com>
2023-01-28 15:54:14 +0530

    array concept using STL

---
+++ array_stl.cpp
@@ -0,0 +1,25 @@
+// standard template libaries - array , vector, deque , list
+
+#include<iostream>
+#include<array>
+
+using namespace std;
+int main (){
+        int basic[3]={1,2,3};
+        array<int,4> a={1,2,3,4};
+        int size=a.size();
+
+        for (int i = 0; i < size;i++)
+        {
+            cout<<a[i]<<endl;
+        }
+        cout<<"element at 2nd place "<<a.at(2)<<endl;
+        cout<<"empty or not"<< a.empty()<<endl;
+        cout<<"element at first "<<a.front()<<endl;
+        cout<<"element at last"<<a.back()<<endl;
+
+
+
+    return 0;
+}
+
