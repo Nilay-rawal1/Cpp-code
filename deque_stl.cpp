7a59c772e83a4d1c4670f7c70dceb112ec770c3c
Nilay Rawal <rawalnilay1@gmail.com>
2023-01-31 22:13:48 +0530

    main in

--- deque_stl.cpp
+++ deque_stl.cpp
@@ -1,3 +1,17 @@
 // push n pop of element on both starting n ending index of array 
 //it is dynamic & random acsess is possible
 // doubleendedque
+#include<iostream>
+#include<deque>
+
+using namespace std;
+
+int main(){
+
+
+
+
+
+
+    return 0;
+}
