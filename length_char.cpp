9968a8b0a4ce74433c5dd394f1a38582a3102a76
Nilay Rawal <rawalnilay1@gmail.com>
2023-02-14 06:34:29 +0530

    length of string

---
+++ length_char.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int getlength(char name[]){
+    int count =0;
+    for (int i = 0; name[i] !='\0'; i++)
+    {
+        count ++;
+
+    }
+    return count;
+    
+    
+}
+
+int main(){
+    char name[20];
+    cout<<"enter your name :?"<<endl;
+    cin>>name;
+
+    cout<<"your name is ";
+    cout<<name<<endl;
+    cout<<"length = "<<getlength(name)<<endl; 
+
+
+return 0;
+
+}
