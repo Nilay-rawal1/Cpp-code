26209c6810d6cb7bd7b9db1327cf0e871466c1bc
Nilay Rawal <rawalnilay1@gmail.com>
2023-02-14 06:34:11 +0530

    intro to char array

---
+++ intro_char_array.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    char name[20];
+    cout<<"enter your name :?"<<endl;
+    cin>>name;
+
+    cout<<"your name is ";
+    cout<<name<<endl;
+
+
+return 0;
+
+}
