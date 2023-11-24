499b4104b92977fa7b23a55a276894c752158b2d
Nilay Rawal <98951434+Nilay-rawal1@users.noreply.github.com>
2023-11-24 16:38:21 +0530

    Create 204_count_Primes_LC.cpp

---
+++ 204_count_Primes_LC.cpp
@@ -0,0 +1,22 @@
+// LeetCode problem:204:- Count Prime numbers,
+// with time complexicity of o(log(log(n))
+class Solution {
+public:
+    int countPrimes(int n) {
+    
+        //Sieve of Eratosthenes
+        int cnt=0;
+        vector<bool>prime(n+1,true);
+        prime[0]=prime[1]=false;
+        for(int i=2;i<n;i++){
+            if(prime[i]){
+                cnt++;
+                for(int j=2*i;j<n;j=j+i){
+                    prime[j]=0;
+                }
+            }
+        }
+        return cnt;
+        
+    }
+};
