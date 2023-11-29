1bd15c7e42742dfd9114f1103031379ba1fb3af3
Nilay <rawalnilay1@gmail.com>
2023-11-30 01:26:21 +0530

    reference varible example 1

---
+++ reference_var_e1.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void update(int a ){
+    a++;
+}
+
+
+
+
+int main(){
+
+    /*int i=5;
+    //create a ref variable int& j=i
+    int& j=i;
+    cout<<i<<endl;
+    i++;
+    cout<<i<<endl;
+    j++;
+    cout<<j<<endl;*/
+
+int n=5;
+cout<<"before "<<n<<endl;
+update(n);
+cout<<"after "<<n;
+
+
+
+
+
+    return 0;
+}
