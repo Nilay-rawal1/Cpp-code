28e95365c6db3502746a006bdb735b4c32385085
Nilay <rawalnilay1@gmail.com>
2023-12-01 16:50:35 +0530

    dynamic 2d array with release memory

---
+++ 2D_dynamic_array.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+int n;
+cin>>n;
+int **arr=new int*[n];
+//dynamic 2d array creation
+for (int i = 0; i < n; i++)
+{
+    arr[i]=new int[n];
+}
+//taking input
+for(int i=0;i<n;i++){
+    for (int j = 0; j < n; j++)
+    {
+        cin>>arr[i][j];
+    }
+    
+
+}
+//output
+for(int i=0;i<n;i++){
+    for (int j = 0; j < n; j++)
+    {
+        cout<<arr[i][j]<<" ";
+    }
+    cout<<endl;
+}
+//releasing memory
+
+for (int i = 0; i < n; i++)
+{
+    delete [] arr[i];
+}
+
+delete []arr;
+
+
+    return 0;
+}
