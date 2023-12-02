2c364ae1cdacb8706934f7d7855e435592ac49bd
Nilay <rawalnilay1@gmail.com>
2023-12-02 22:05:20 +0530

    macro in c++

---
+++ mcros.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+//macros using define 
+#define PI 3.14
+
+
+int main(){
+
+    int r=5;
+    double area=PI*r*r;
+
+    cout<<"Area is : "<<area<<endl;
+
+
+    return 0;
+
+}
