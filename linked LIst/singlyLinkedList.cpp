b423c5f0a7bf8d98d23822e8bdbfcee00e6e1694
Nilay <rawalnilay1@gmail.com>
2024-01-09 19:56:01 +0530

    insertion at any position

--- linked LIst/singlyLinkedList.cpp
+++ linked LIst/singlyLinkedList.cpp
@@ -13,12 +13,13 @@ public:
         this->next = NULL;
     }
 };
-//insert at tail ()
-void insertAtTail(Node* &tail,int d){
-    Node *temp = new Node(d);
-    tail->next=temp;
-    tail= temp;
 
+// insert at tail ()
+void insertAtTail(Node *&tail, int d)
+{
+    Node *temp = new Node(d);
+    tail->next = temp;
+    tail = temp;
 }
 
 // insertion at head()
@@ -28,6 +29,34 @@ void InsertAtHead(Node *&head, int d)
     Node *temp = new Node(d);
     temp->next = head;
     head = temp;
+}
+// insertion At any position
+void InsertAtPosition(Node *&tail, Node *&head, int position, int d)
+{
+    // starting point
+    if (position == 1)
+    {
+        InsertAtHead(head, d);
+        return;
+    }
+    // updating tail
+
+    Node *temp = head;
+    int cnt = 1;
+    while (cnt < position - 1)
+    {
+        temp = temp->next;
+        cnt++;
+    }
+
+    if (temp->next == NULL)
+    {
+        insertAtTail(head, d);
+        return;
+    }
+    Node *nodeToInsert = new Node(d);
+    nodeToInsert->next = temp->next;
+    temp->next = nodeToInsert;
 }
 // traversing and printing
 
@@ -47,7 +76,7 @@ void Print(Node *&head)
 // driver code
 int main()
 {
- 
+
     // created a new note
     Node *node1 = new Node(10);
 
@@ -56,13 +85,17 @@ int main()
     // head pointed to node1
 
     Node *head = node1;
-    Node* tail=node1;
+    Node *tail = node1;
     Print(head);
     // insertion
     insertAtTail(tail, 12);
     Print(head);
     insertAtTail(tail, 15);
     Print(head);
+    InsertAtPosition(tail, head, 1, 22);
+    Print(head);
+    InsertAtPosition(tail, head, 4, 24);
+    Print(head);
 
-    return 0; 
+    return 0;
 }
