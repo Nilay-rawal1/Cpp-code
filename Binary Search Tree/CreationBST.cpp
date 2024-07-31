62531e7f86baa517af13b609567a14e1bd70925f
Nilay <rawalnilay1@gmail.com>
2024-07-31 17:44:43 +0530

    deletion of node in LOT

--- Binary Search Tree/CreationBST.cpp
+++ Binary Search Tree/CreationBST.cpp
@@ -101,6 +101,57 @@ Node* maxval(Node*root){
 }
 
 
+
+Node* deleteFromBST(Node* root,int val){
+        //base case
+        if(root==NULL)
+        return root;
+
+        if(root->data==val){
+            //0 child
+            if(root->left==NULL && root->right==NULL){
+                delete root;
+                return NULL;
+            }
+
+            //1child
+                    //left child
+                if(root->left !=NULL && root->right ==NULL){
+                    Node* temp=root->left;
+                    delete root;
+                    return temp;
+                }
+                    //right child
+                if(root->left ==NULL && root->right!=NULL){
+                        Node* temp=root->left;
+                        delete root;
+                        return temp;
+
+                }
+
+            //2child
+            if(root->left !=NULL && root->right !=NULL ){
+                int mini = minval(root->right)->data;
+                root->data=mini;
+                root->right=deleteFromBST(root->right,mini);
+                return root;
+            }
+
+
+        }
+        else if (root->data>val){
+            root->left=deleteFromBST(root->left,val);
+                return root;
+        }
+        else{
+            root->right=deleteFromBST(root->right,val);
+            return root;
+
+        }
+
+}
+
+
 void takeInput(Node *&root)
 {
     int data;
@@ -125,7 +176,15 @@ int main()
     cout<<endl<<"<----- Min and Max Values ----->"<<endl;
     cout<<"min value : "<< minval(root)->data;
     cout<<endl;
-    cout<<"max value : "<< maxval(root)->data;
+    cout<<"max value : "<< maxval(root)->data<<endl;
+    cout<<"enter the value you want to delete:";
+    int val;
+    cin>>val;
+     root= deleteFromBST(root,val);
+
+    cout <<endl<< "Printing the BST  after deleteion:" << endl;
+    levelOrderTraversal(root);
+
 
     return 0;
 }
