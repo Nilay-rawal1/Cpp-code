d3d498dbe55faa6ff241598e08c6aef40169b7d1
Nilay <rawalnilay1@gmail.com>
2024-08-05 17:34:34 +0530

    heapify creation

--- Heaps/creation_heap.cpp
+++ Heaps/creation_heap.cpp
@@ -83,6 +83,27 @@ class heap{
 
 };
 
+void heapify(int arr[],int n,int i){
+    int largest=i;
+    int left=2*i;
+    int right=2*i+1;
+    if(left<n && arr[largest]<arr[left]){
+
+            largest=left;
+    }
+    if(right<n &&arr[largest]<arr[right]){
+        largest =right;
+
+    }
+    if(largest !=i){
+        swap(arr[largest],arr[i]);
+        heapify(arr,n,largest);
+    
+    }
+
+
+}
+
 
 
 
@@ -103,6 +124,21 @@ heap h ;
     h.deletefromheap();
     cout<<endl<<"-----after deletion-----"<<endl;
     h.print();
+    
+    int arrs[6]={-1,54,53,55,53,50};
+    int n=5;
+    for (int i = n/2; i =0; i--)
+    {
+        heapify(arrs,5,i);
+    }
+    cout<<"----- printing the array using heapify-----"<<endl;
+    for(int i=1;i<=n;i++){
+        cout<<arrs[i]<<" ";
+
+    }cout<<endl;
+
+
+    
     return 0;
 
 }
