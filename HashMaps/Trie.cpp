87444fd313588531a59fcb86f8b084b00f361653
Nilay <rawalnilay1@gmail.com>
2024-08-13 15:27:38 +0530

    trie

---
+++ HashMaps/Trie.cpp
@@ -0,0 +1,87 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class TrieNode {
+public:
+    char data;
+    TrieNode* children[26];
+    bool isTerminal;
+
+    TrieNode(char ch) {
+        data = ch;
+        for(int i = 0; i < 26; i++) {
+            children[i] = NULL;
+        }
+        isTerminal = false;
+    }
+};
+
+class Trie {
+public:
+    TrieNode* root;
+
+    Trie() {
+        root = new TrieNode('\0');
+    }
+
+    void insertUtil(TrieNode* root, string word) {
+        // base case
+        if(word.length() == 0) {
+            root->isTerminal = true;
+            return;
+        }
+
+        // assumption word will be in lowercase
+        int index = word[0] - 'a';
+        TrieNode* child;
+
+        if(root->children[index] != NULL) {
+            child = root->children[index];
+        } else {
+            // absent
+            child = new TrieNode(word[0]);
+            root->children[index] = child;
+        }
+
+        // recursion
+        insertUtil(child, word.substr(1));
+    }
+
+    void insertWord(string word) {
+        insertUtil(root, word);
+    }
+
+    bool searchUtil(TrieNode* root, string word) {
+        // base case
+        if(word.length() == 0) {
+            return root->isTerminal;
+        }
+
+        int index = word[0] - 'a';
+        TrieNode* child;
+
+        // present
+        if(root->children[index] != NULL) {
+            child = root->children[index];
+        } else {
+            // absent
+            return false;
+        }
+
+        // recursion
+        return searchUtil(child, word.substr(1));
+    }
+
+    bool searchWord(string word) {
+        return searchUtil(root, word);
+    }
+};
+
+int main() {
+    Trie* t = new Trie();
+    t->insertWord("abcd");
+
+    cout << "Present or Not: " << t->searchWord("abcdw") << endl;
+
+    return 0;
+}
